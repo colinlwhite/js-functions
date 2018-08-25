f3550d5987d33d997032c79cfab288957131c1a8
Colin White <clwhite814@gmail.com>
2018-08-25 11:49:02 -0500

    attempted a writeToDom

--- functions.js
+++ functions.js
@@ -29,3 +29,35 @@ const bearSticks = nuggetizer('bear');
 
 console.log(nomnom('colin', bearSticks)); // colin devoured bear sticks 
 
+
+// Function numberAdder that takes in a number and returns the number plus 3. so input=22 output = 25
+// ES5 
+
+function numberAdder(numberOne) {
+    return numberOne + 3;
+};
+
+console.log(numberAdder(22));
+
+// ES6 
+
+const printToDom = (stringToPrint, divId) => {
+    const selectedDiv = docuement.getElementById(divId);
+    selectedDiv.innerHTML += stringToPrint;
+}; 
+
+const numberAdder1 = (nums) => {
+     const finalNumber = nums + 3;
+     printToDom(`<h2>${finalNumber}</h2>`, 'allTheNumbers'); 
+ };
+
+console.log('num: ', numberAdder1(22)); // 25 
+
+
+numberAdder1(22);
+numberAdder1(2);
+numberAdder1(4);
+
+
+
+
